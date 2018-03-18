89e5aac7912b2fc82c843995b57e3b9cb4bfd238
Shiksha <shiksha.chauhan@traveltriangle.com>
2018-03-18 14:59:57 +0530

    Add flow chart diagram

--- lib/search_service.rb
+++ lib/search_service.rb
@@ -6,12 +6,16 @@ class SearchService
 
   # Task 1 - This is calling up the search service
   def search_location(user_lat, user_long, keyword)
-    location_search = LocationSearch.new(user_lat, user_long, keyword)
-    location_search.load
-    if location_search.autocomplete_list.empty?
-      "No Results found"
-    else
-      location_search.autocomplete_list
+    begin
+      location_search = LocationSearch.new(user_lat, user_long, keyword)
+      location_search.load
+      if location_search.autocomplete_list.empty?
+        "No Results found"
+      else
+        location_search.autocomplete_list
+      end
+    rescue => e
+      "Something went Wrong !! #{e.inspect}"
     end
   end
 end
